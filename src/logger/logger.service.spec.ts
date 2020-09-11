7e571256cc53cac23781567c60caa3c72fa386e8
hantsy <hantsy@gmail.com>
2020-09-11 17:36:28 +0800

    test: improve logger.service.sepc.ts

--- src/logger/logger.service.spec.ts
+++ src/logger/logger.service.spec.ts
@@ -15,4 +15,17 @@ describe('LoggerService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('log', () => {
+    const consoleSpy = jest.spyOn(global.console, 'log');
+    service.log("hello");
+    expect(consoleSpy).toBeCalledWith('hello');
+  });
+
+  it('log with prefix', () => {
+    const consoleSpy = jest.spyOn(global.console, 'log');
+    service.setPrefix("H")
+    service.log("hello");
+    expect(consoleSpy).toBeCalledWith('[H] hello');
+  });
 });
